4714e6e6a57a3a3db68890c7406dbd509ade6e32
shaohua_school <shaohua@bestapp.us>
2015-04-07 18:08:42 +0800

    回退

--- Project.cpp
+++ Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	OpenmeshHelper mesh_helper("D:/lena_s.jpg");
 
-	mesh_helper.ReduceVertices(0.95, true);
+	mesh_helper.ReduceVertices(0.25, false);
 	mesh_helper.Output("D:/output.off");
 	system("pause");
 	return 0;
